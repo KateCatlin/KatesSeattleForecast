9bcaaf9e38b586d1ce31d58d0dc49064da3c5222
Kate Catlin <kcatlin2@gmail.com>
2024-11-11 20:49:16 -0800

    Add in time until sunset

--- ping_sunset.py
+++ ping_sunset.py
@@ -1,6 +1,8 @@
 import requests
 import json
 from datetime import datetime
+import pytz
+from datetime import datetime, timedelta
 import os
 
 def fetch_sunset_data():
@@ -16,10 +18,33 @@ def fetch_sunset_data():
         
         # Format sunset time to remove seconds
         if data.get('results', {}).get('sunset'):
-            # Parse the time string
+            # Parse the sunset time
             sunset_time = datetime.strptime(data['results']['sunset'], '%I:%M:%S %p')
-            # Format to hours and minutes only
+            
+            # Get current time in Seattle
+            seattle_tz = pytz.timezone('America/Los_Angeles')
+            current_time = datetime.now(seattle_tz)
+            
+            # Set sunset time to today's date
+            sunset_datetime = current_time.replace(
+                hour=sunset_time.hour,
+                minute=sunset_time.minute,
+                second=0,
+                microsecond=0
+            )
+            
+            # Format sunset time (HH:MM)
             data['results']['sunset'] = sunset_time.strftime('%I:%M %p')
+            
+            # Calculate time until sunset
+            if current_time < sunset_datetime:
+                time_diff = sunset_datetime - current_time
+                hours = time_diff.seconds // 3600
+                minutes = (time_diff.seconds % 3600) // 60
+                time_until = f"{hours} hours and {minutes} minutes until sunset"
+                data['results']['time_until_sunset'] = time_until
+            else:
+                data['results']['time_until_sunset'] = 'after_sunset'
         
         # Save to a JSON file
         with open('sunset.json', 'w') as f:
